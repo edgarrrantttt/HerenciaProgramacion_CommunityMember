eff324dfd790927eb849cb72543a7f4bdc712f68
Edgar Antonio Rosario de la Rosa <20241678@itla.edu.do>
2025-09-21 20:45:07 -0400

    Corregido

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace HerenciaProgramacion
             ExStudent exstudent = new ExStudent("Eitan", "Guzman", "M", "2023/02/12", "Ex-Student", "Cibersecurity");
             Console.WriteLine(exstudent.ExStudentInfo());
 
-            Employee newemployee = new Employee("Edward", "Feliz", "F", " ", 25400);
+            Employee newemployee = new Employee("Edward", "Feliz", "F","Secretary",25400);
             Console.WriteLine(newemployee.EmployeeInfo());
 
             Executive newexcutive = new Executive("Ramona", "Villanueba", "F", "Technian", 50000, "IT", "ramonait@itla.it.do");
@@ -22,7 +22,7 @@ namespace HerenciaProgramacion
             Lecturer newlecturer = new Lecturer("Antonio", "Rosa", "M", "Lecturer", 75000, "Math");
             Console.WriteLine(newlecturer.ShowLecturer());
 
-            Teacher newteacher = new Teacher("Francisco", "Gomez", "M", "Teacher", 67000, "Math", "Algebra");
+            Teacher newteacher = new Teacher("Francisco", "Gomez", "M", "Teacher", 67000, "History", "B001");
             Console.WriteLine(newteacher.InfoTeacher());
             
         }
